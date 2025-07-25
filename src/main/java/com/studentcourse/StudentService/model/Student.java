2feadb1b01c73898a2218bd1c6bfa11e3fd8ac18
r9988.2588 <r9988.2588@gmail.com>
2025-07-25 04:23:27 +0000

    updated student model

--- src/main/java/com/studentcourse/StudentService/model/Student.java
+++ src/main/java/com/studentcourse/StudentService/model/Student.java
@@ -1,6 +1,9 @@
 package com.studentcourse.StudentService.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -8,6 +11,9 @@ import lombok.Data;
 @Data
 @Table(name = "students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fullname;
     private String email;
