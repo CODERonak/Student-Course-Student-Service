62392ce81652ac11966daa56657ee76b989729e5
r9988.2588 <r9988.2588@gmail.com>
2025-07-25 04:56:37 +0000

    done with the student controller

---
+++ src/main/java/com/studentcourse/StudentService/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.studentcourse.StudentService.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.studentcourse.StudentService.StudentService;
+import com.studentcourse.StudentService.dto.StudentRequest;
+import com.studentcourse.StudentService.dto.StudentResponse;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/students")
+@AllArgsConstructor
+public class StudentController {
+    private final StudentService service;
+
+    @PostMapping
+    public ResponseEntity<StudentResponse> addStudent(@RequestBody StudentRequest request) {
+        StudentResponse response = service.addStudent(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentResponse>> getAllStudents() {
+        List<StudentResponse> response = service.getAllStudents();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+}
